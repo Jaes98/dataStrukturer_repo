9703d0a2a3a16f957a5b62bd40e12975d0e78319
Jaes98 <marcus.jaes@gmail.com>
2024-09-16 00:51:47 +0200

    lavet metoder jeg kunne lave hurtigt, to be continued

--- LinkedLists/DoublyLinkedList/DoublyLinkedList.js
+++ LinkedLists/DoublyLinkedList/DoublyLinkedList.js
@@ -7,93 +7,132 @@ export default class DoublyLinkedList {
   }
 
   addLast(data) {
-
+    const newNode = new Node(data);
+    this.addNodeLast(newNode);
   }
-
   addFirst(data) {
-  
+    const newNode = new Node(data);
+    this.addNodeFirst(newNode);
   }
-
-  get() {
-
+  get(index) {
+    return this.getNode(index).data;
   }
 
   indexOf(data) {
-
+    let node = this.head;
+    let index = 0;
+    while (node) {
+      if (node.data === data) {
+        return index;
+      }
+      index++;
+      node = node.next;
+    }
+    return -1;
   }
 
-  insertAfter( data, after) {
+  insertAfter(index, data) {
+    const preExisting = this.nodeAt(index);
+    if (preExisting) {
+      this.insertAfterNode(new Node(data), preExisting);
+    } else {
+      console.log("no node found at the index");
+      return;
+    }
   }
 
-  insertBefore(data, before) {
+  insertBefore(index, data) {
+    const preExisting = this.nodeAt(index);
+    if (preExisting) {
+      this.insertBeforeNode(new Node(data), preExisting);
+    } else {
+      console.log("no node found at the index");
+      return;
+    }
   }
 
   first() {
-
+    return this.head;
   }
 
   last() {
-
+    return this.tail;
   }
 
   remove(data) {
-
+    const node = this.getNodeWith(data);
+    if (node) {
+      this.removeNode(node);
+    } else {
+      console.log("no node found with the data");
+      return;
+    }
   }
 
-  removeIndex(index) {
-
-  }
+  removeIndex(index) {}
 
   removeFirst() {
-
-  }
+    if (this.head) {
+      this.head = this.head.next;
+      this.head.prev = null
+      }
+    }
 
   removeLast() {
-
+    if (this.tail) {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+    }
   }
 
   getNode(index) {
-
+    let node = this.head;
+    for (let i = 0; i < index; i++) {
+      node = node.next;
+    }
+    return node;
   }
 
-  addNodeLast(node) {
-  }
+  addNodeLast(node) {}
 
-  addNodeFirst(node) {
-  }
+  addNodeFirst(node) {}
 
-  insertAfterNode(node, after) {
-  }
+  insertAfterNode(node, after) {}
 
-  insertBeforeNode(node, before) {
-  }
+  insertBeforeNode(node, before) {}
 
-  removeNode(node) {
-  }
+  removeNode(node) {}
 
-  nodeAt(index) {
-  }
+  nodeAt(index) {}
 
-  swapNodes(node1, node2) {
-  }
+  swapNodes(node1, node2) {}
 
   clear() {
+    this.head = null;
+    this.tail = null;
   }
 
   size() {
+    let node = this.head;
+    let count = 0;
+    while (node) {
+      count++;
+      node = node.next;
+    }
+    return count;
   }
 
   dumpList() {
+    let node = this.head;
+    while (node) {
+      console.log("Node: ", node);
+      node = node.next;
+    }
   }
 
-  getNextNode(node) {
-  }
+  getNextNode(node) {}
 
-  getPreviousNode(node) {
-  }
-
-  
-
+  getPreviousNode(node) {}
 }
 
 export class Node {
